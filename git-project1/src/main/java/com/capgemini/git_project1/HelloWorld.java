12a34e69d97f5a3fbed59cd0438d1017783fe110
priyal1102 <21512476.dypit@dypvp.edu.in>
2025-05-12 12:54:59 +0530

    second commit

--- git-project1/src/main/java/com/capgemini/git_project1/HelloWorld.java
+++ git-project1/src/main/java/com/capgemini/git_project1/HelloWorld.java
@@ -3,6 +3,6 @@ package com.capgemini.git_project1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World1");
 	}
 }
