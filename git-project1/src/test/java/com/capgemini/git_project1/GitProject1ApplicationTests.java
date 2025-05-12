3d962e1870c3b4b785c5b20d532c1c03f49df0fd
priyal1102 <21512476.dypit@dypvp.edu.in>
2025-05-12 12:20:59 +0530

    Commit All files

---
+++ git-project1/src/test/java/com/capgemini/git_project1/GitProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.git_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
